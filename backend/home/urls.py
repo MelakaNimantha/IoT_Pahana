d5d7a190d1aa831dce8d205cdbaff4216bd0fd2e
Akila <akilamatrix@gmail.com>
2018-05-21 11:45:42 +0530

    python powered site

---
+++ backend/home/urls.py
@@ -0,0 +1,31 @@
+"""backend URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signupreq/', views.signupreq, name='signupreq'),
+    path('signinreq/', views.signinreq, name='signinreq'),
+    path('signout/', views.signout, name="signout"),
+    path('menu/', views.menu, name="menu"),
+    path('menu/lightup/', views.lightup),
+    path('menu/lightup/on', views.lightOn),
+    path('menu/lightup/off', views.lightOff),
+]
---
+++ backend/home/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+def home(request):
+    if not(request.user.is_anonymous):
+        return redirect("menu")
+    else:
+        return render(request, 'home.html')
+
+def signup(request):
+    if 'eMessage' in request.session.keys():
+        page = render(request, "signup.html", {"error":request.session['eMessage']})
+        del request.session['eMessage']
+    else:
+        page = render(request, "signup.html")
+    return page
+
+def signin(request):
+    if 'eMessage' in request.session.keys():
+        page = render(request, "login.html", {"error":request.session['eMessage']})
+        del request.session['eMessage']
+    else:
+        page = render(request, "login.html")
+    return page
+
+def signupreq(request):
+    username = request.POST['username'].strip()
+    u, created = User.objects.get_or_create(username=username)
+    if created:
+        first_name = request.POST['first_name'].strip()
+        last_name = request.POST['last_name'].strip()
+        password = request.POST['password'].strip()
+
+        u.set_password(password)
+        u.first_name = first_name
+        u.last_name = last_name 
+
+        u.save()
+        return redirect("signin")
+    else:
+        request.session['eMessage'] = "Username already exists."
+        return redirect('signup')
+
+def signinreq(request):
+    username = request.POST['username'].strip()
+    password = request.POST['password'].strip()
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect("menu")
+    else:
+        request.session['eMessage'] = "Invalid authentication data"
+        return redirect("signin")
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def menu(request):
+    if request.user.is_anonymous:
+        return redirect('signin')
+    else:
+        return render(request, "menu.html", {'user': request.user})
+
+def lightup(request):
+    if request.user.is_anonymous:
+        return redirect('signin')
+    else:
+        return render(request, "light.html")
+
+def lightOn(request):
+    #do what ever message passing to your device
+    print("on")
+    return render(request, "light.html", {'status': 'on'})
+
+def lightOff(request):
+    #do what ever message passing to your device
+    print("off")
+    return render(request, "light.html", {'status': 'off'})
